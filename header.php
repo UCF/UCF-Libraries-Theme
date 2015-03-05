6507765b8b5b386f47deb3c90bc023867658f560
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-03-05 11:32:47 -0500

    Updated header menu

--- header.php
+++ header.php
@@ -18,18 +18,20 @@
 	</head>
 	<body <?php body_class(); ?>>
 	<header>
-			<nav class="navbar navbar-static-top" role="navigation"> 
-				<div class="container">
-				<!-- Brand and toggle get grouped for better mobile display --> 
-				  <div class="navbar-header"> 
-				    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse"> 
-				      <span class="sr-only">Toggle navigation</span> 
-				      <span class="icon-bar"></span> 
-				      <span class="icon-bar"></span> 
-				      <span class="icon-bar"></span> 
-				    </button> 
+		<nav class="navbar navbar-library navbar-static-top" role="navigation">
+			<div class="container">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button class="navbar-toggle" data-target=".navbar-collapse" data-toggle="collapse" type="button">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-text">Menu</span>
+					</button>
+
 				    <a class="navbar-brand" href="<?php bloginfo('url')?>"><?php bloginfo('name')?></a>
-				  </div> 
+				</div> 
 				  <!-- Collect the nav links, forms, and other content for toggling --> 
 				    <?php /* Primary navigation */
 						wp_nav_menu( array(
@@ -37,7 +39,7 @@
 						  'depth'				=> 2,
 						  'container'			=> 'div',
 						  'container_class'		=> 'collapse navbar-collapse',
-						  'menu_class' 			=> 'nav nav-pills navbar-right',
+						  'menu_class' 			=> 'nav navbar-nav navbar-right',
 						  'fallback_cb'      	=> 'wp_bootstrap_navwalker::fallback',
 						  //Process nav menu using our custom nav walker
 						  'walker' => new wp_bootstrap_navwalker())
