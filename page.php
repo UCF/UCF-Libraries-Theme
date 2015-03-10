b358914b7ef89391b7bc3dcdf8c446e9fb2ffadb
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-03-10 15:09:45 -0400

    Reorganized sidebar into bootstrap column config.

--- page.php
+++ page.php
@@ -9,12 +9,16 @@ Description: Default Page.
 <?php get_header(); ?>
 <div id="main">
 	<div id="content" class="container">
-		<?php get_sidebar(); ?>
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<h1><?php the_title(); ?></h1>
-		<p><?php the_content(__('(more...)')); ?></p>
-		<hr> <?php endwhile; else: ?>
-		<p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
+		<div class="col-sm-3">
+			<?php get_sidebar(); ?>
+		</div>
+		<div class="col-sm-9">
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<h1><?php the_title(); ?></h1>
+			<p><?php the_content(__('(more...)')); ?></p>
+			<hr> <?php endwhile; else: ?>
+			<p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
+		</div>
 	</div>
 </div>
 <div id="delimiter"></div>
