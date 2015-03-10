cb83b456b6dfc8b31899616fb012e6bc7c554e4d
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-03-10 14:57:25 -0400

    Adding in the footer with static html. Revisit for wordpress implementation

--- footer.php
+++ footer.php
@@ -1,3 +1,64 @@
+<footer>
+	<div class="container">
+		<div class="col-sm-3 Find">
+			<ul>
+				<li class="footer-title"><a href="">
+				<span class="glyphicon glyphicon-search"></span>&nbsp;Find</a></li>
+				<li><a href="">Books</a></li>
+				<li><a href="">Articles</a></li>
+				<li><a href="">Research Guides</a></li>
+				<li><a href="">Collections</a></li>
+				<li><a href="">Reserves</a></li>
+				<li><a href="">Videos/Media</a></li>
+				<li><a href="">Computers</a></li>
+				<li><a href="">Study Rooms</a></li>
+			</ul>
+		</div>
+		<div class="col-sm-3 Services">
+			<ul>
+				<li class="footer-title"><a href="">
+				<span class="glyphicon glyphicon-education"></span>&nbsp;Services</a></li>
+				<li><a href="">Borrowing from other libraries</a></li>
+				<li><a href="">Instruction</a></li>
+				<li><a href="">Disability Services</a></li>
+				<li><a href="">Print / Copy / Scan</a></li>
+				<li><a href="">Build Our Collection</a></li>
+				<li><a href="">Technology in the Library</a></li>
+			</ul>
+		</div>
+		<div class="col-sm-3 About">
+			<ul>
+				<li class="footer-title"><a href="">
+				<span class="glyphicon glyphicon-info-sign"></span>&nbsp;About</a></li>
+				<li><a href="">Hours &amp; Locations</a></li>
+				<li><a href="">Contact</a></li>
+				<li><a href="">Departments </a></li>
+				<li><a href="">Directory</a></li>
+				<li><a href="">Jobs </a></li>
+				<li><a href="">Policies</a></li>
+				<li><a href="">News &amp; Events</a></li>
+				<li><a href="">Site Map</a></li>
+			</ul>
+		</div>
+		<div class="col-sm-3 Help">
+			<ul>
+				<li class="footer-title"><a href="">
+				<span class="glyphicon glyphicon-question-sign"></span>&nbsp;Let 
+				US Help</a></li>
+				<li><a href="">Ask Us </a></li>
+				<li><a href="">Schedule an appointment</a></li>
+				<li><a href="">Your Librarian (campus &amp; subject)</a></li>
+				<li class="footer-title"><a href="">
+				<span class="glyphicon glyphicon glyphicon-user"></span>&nbsp;My 
+				Account</a></li>
+				<li><a href="">Catalog</a></li>
+				<li><a href="">ILL</a></li>
+				<li><a href="">Articles</a></li>
+				<li><a href="">Fines</a></li>
+			</ul>
+		</div>
+	</div>
+</footer>
 <?php wp_footer(); ?>
 	</body>
 </html>
