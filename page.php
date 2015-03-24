94ea0380a9effa02d767f7f38a5330eb35aa783b
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-03-24 12:17:26 -0400

    changed the position of h1 elements to the very top of the page under the header. Added in alignment css that should work with standard wordpress classes

--- page.php
+++ page.php
@@ -9,12 +9,12 @@ Description: Default Page.
 <?php get_header(); ?>
 <div id="main">
 	<div id="content" class="container">
+		<h1><?php the_title(); ?></h1>
 		<div class="col-sm-3">
 			<?php get_sidebar(); ?>
 		</div>
 		<div class="col-sm-9">
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			<h1><?php the_title(); ?></h1>
 			<p><?php the_content(__('(more...)')); ?></p>
 			<hr> <?php endwhile; else: ?>
 			<p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
