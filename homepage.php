08c51baab8c39e081228d3fbbcb71e7351c97e3c
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-03-25 10:55:12 -0400

    removed h1 title

--- homepage.php
+++ homepage.php
@@ -8,7 +8,6 @@ Description: A Template for the Website homepage.
 <?php get_header(); ?>
 <div id="main">
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<h1><?php the_title(); ?></h1>
 		<h4>Posted on <?php the_time('F jS, Y') ?></h4>
 		<p><?php the_content(__('(more...)')); ?></p>
 		<hr> <?php endwhile; else: ?>
