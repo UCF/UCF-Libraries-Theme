47033a1d296f71677349cd257f80752257d3fcf5
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-03-26 14:58:46 -0400

    adding in  to shortcodes

--- functions.php
+++ functions.php
@@ -82,7 +82,7 @@ function OneSearchform( $form ) {
 add_shortcode('OneSearch', 'OneSearchform');
 
 // Homepage Searchbox Shortcode
-function HomepageSearchBox( $content = null ) {
+function HomepageSearchBox( $atts, $content = null ) {
   return '<div id="tabs">
               <ul>
                 <li><a href="#QuickSearch"><span>QuickSearch</span></a></li>
@@ -96,53 +96,37 @@ function HomepageSearchBox( $content = null ) {
           $( "#tabs" ).tabs();
           </script>';
 }
-
 add_shortcode('HomepageSearch', 'HomepageSearchBox');
 
 
 // QuickSearch Tab 
-function QuickSearchTab( $content = null ) {
-  return '<div id="QuickSearch">'
-          . do_shortcode($content) .
-          '</div>';
+function QuickSearchTab( $atts, $content = null ) {
+  return '<div id="QuickSearch">'.do_shortcode($content).'</div>';
 }
-
 add_shortcode('QuickSearch', 'QuickSearchTab');
 
 // Articles Tab 
-function ArticlesTab( $content ) {
-  return '<div id="Articles">'
-          . do_shortcode($content) .
-          '</div>';
+function ArticlesTab( $atts, $content = null ) {
+  return '<div id="Articles">'.do_shortcode($content).'</div>';
 }
-
 add_shortcode('Articles', 'ArticlesTab');
 
 // Books Tab 
-function BooksTab( $content = null ) {
-  return '<div id="Books">'
-          . do_shortcode($content) .
-          '</div>';
+function BooksTab( $atts, $content = null ) {
+  return '<div id="Books">'.do_shortcode($content).'</div>';
 }
-
 add_shortcode('Books', 'BooksTab');
 
 // Videos Tab 
-function VideosTab( $content = null ) {
-  return '<div id="Videos">'
-          . do_shortcode($content) .
-          '</div>';
+function VideosTab( $atts, $content = null ) {
+  return '<div id="Videos">'.do_shortcode($content).'</div>';
 }
-
 add_shortcode('Videos', 'VideosTab');
 
 // Website Tab 
-function WebsiteTab( $content = null ) {
-  return '<div id="Website">'
-          . do_shortcode($content) .
-          '</div>';
+function WebsiteTab( $atts, $content = null ) {
+  return '<div id="Website">'.do_shortcode($content).'</div>';
 }
-
 add_shortcode('Website', 'WebsiteTab');
 
 ?>
