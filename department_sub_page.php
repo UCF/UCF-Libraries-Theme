d912b7592fedc41a755e0583658e42d6a742d964
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-03-27 14:07:50 -0400

    added in a link to the parent page h1

--- department_sub_page.php
+++ department_sub_page.php
@@ -9,7 +9,8 @@ Description: Use for all department sub pages.
 <?php get_header(); ?>
 <div id="main">
 	<div id="content" class="container">
-		<h1><?php echo get_the_title($post->post_parent);?></h1>
+		<?php $permalink = get_permalink($post->post_parent); ?>
+		<h1><a href="<?php echo $permalink; ?>"><?php echo get_the_title($post->post_parent);?></a></h1>
 		<div class="row">
 			<div class="col-sm-3">
 				<?php get_sidebar(); ?>
