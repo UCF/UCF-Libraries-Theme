1de55f3e2e124b128b018de2386062de6e6b7dd4
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-04-03 15:53:25 -0400

    copied over code from index to try and fix

--- single-staff.php
+++ single-staff.php
@@ -9,16 +9,18 @@ Description: Single staff member page.
 <?php get_header(); ?>
 <div id="main">
 	<div id="content" class="container">
-		
+		<p>single-staff template.</p>
 		<div class="row">
 			<div class="col-sm-3">
 				<?php get_sidebar(); ?>
 			</div>
 			<div class="col-sm-9">
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 				<h1><?php the_title(); ?></h1>
 				<p><?php the_post_thumbnail( ); ?></p>
-				<p><?php echo get_the_term_list( $post->ID, 'department', 'Department: ', ', ', '' ); ?></p>
 				<p><?php the_content(__('(more...)')); ?></p>
+				<hr> <?php endwhile; else: ?>
+				<p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
 			</div>
 		</div>
 	</div>
