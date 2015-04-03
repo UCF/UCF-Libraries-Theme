630182776487688bfb66ae04fec14436998aa9f5
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-04-03 15:26:31 -0400

    removed debug code, moved title into loop

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php get_header(); ?>
 <div id="main">
 	<div id="content" class="container">
-	<h1><?php the_title(); ?></h1>
 		<div class="col-sm-3">
 			<?php get_sidebar(); ?>
 		</div>
 		<div class="col-sm-9">
-			<h2>This is the index template.</h2>
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<h1><?php the_title(); ?></h1>
 			<h4>Posted on <?php the_time('F jS, Y') ?></h4>
 			<p><?php the_content(__('(more...)')); ?></p>
 			<hr> <?php endwhile; else: ?>
