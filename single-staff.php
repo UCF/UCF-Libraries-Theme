2adfedddcb64ef864bf32361a5b9bc5f73c959e6
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-04-03 15:48:26 -0400

    fixing thecontent

--- single-staff.php
+++ single-staff.php
@@ -9,15 +9,16 @@ Description: Single staff member page.
 <?php get_header(); ?>
 <div id="main">
 	<div id="content" class="container">
-		<h1><?php the_title(); ?></h1>
+		
 		<div class="row">
 			<div class="col-sm-3">
 				<?php get_sidebar(); ?>
 			</div>
 			<div class="col-sm-9">
+				<h1><?php the_title(); ?></h1>
 				<p><?php the_post_thumbnail( ); ?></p>
 				<p><?php echo get_the_term_list( $post->ID, 'department', 'Department: ', ', ', '' ); ?></p>
-				<?php the_content(); ?>
+				<p><?php the_content(__('(more...)')); ?></p>
 			</div>
 		</div>
 	</div>
