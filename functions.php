b4372479d758b00a20be70ad8ba3dc73a9bda1bd
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-04-03 14:40:01 -0400

    adding in featured image

--- functions.php
+++ functions.php
@@ -268,4 +268,14 @@ function department_init() {
 }
 add_action( 'init', 'department_init' );
 
+//Adding in Featured image feature
+if ( function_exists( 'add_theme_support' ) ) { 
+add_theme_support( 'post-thumbnails' );
+set_post_thumbnail_size( 150, 150, true ); // default Post Thumbnail dimensions (cropped)
+
+// additional image sizes
+// delete the next line if you do not need additional image sizes
+add_image_size( 'category-thumb', 300, 9999 ); //300 pixels wide (and unlimited height)
+
+}
 ?>
