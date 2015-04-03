a6dff2d378fd3ae9d5f6eabe4c49922869ba8dbf
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-04-03 15:37:57 -0400

    overwriting category with contents from index.php

--- category.php
+++ category.php
@@ -1,54 +1,19 @@
-<?php
-/*
-Template Name: Category
-Description: A Template for the categories.
-*/
-
-get_header(); ?> 
-
-<section id="primary" class="site-content">
-<div id="content" role="main">
-
-<?php 
-// Check if there are any posts to display
-if ( have_posts() ) : ?>
-
-<header class="archive-header">
-<h1 class="archive-title">Category: <?php single_cat_title( '', false ); ?></h1>
-
-
-<?php
-// Display optional category description
- if ( category_description() ) : 
-<div class="archive-meta"><?php echo category_description(); ?></div>
-<?php endif; ?>
-</header>
-
-<?php
-
-// The Loop
-while ( have_posts() ) : the_post();
-<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-<small><?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?></small>
-
-<div class="entry">
-<?php the_content(); ?>
-
- <p class="postmetadata"><?php
-  comments_popup_link( 'No comments yet', '1 comment', '% comments', 'comments-link', 'Comments closed');
-?></p>
+<?php get_header(); ?>
+<div id="main">
+	<div id="content" class="container">
+		<div class="col-sm-3">
+			<?php get_sidebar(); ?>
+		</div>
+		<div class="col-sm-9">
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<h1><?php the_title(); ?></h1>
+			<p><?php the_post_thumbnail( ); ?></p>
+			<h4>Posted on <?php the_time('F jS, Y') ?></h4>
+			<p><?php the_content(__('(more...)')); ?></p>
+			<hr> <?php endwhile; else: ?>
+			<p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
+		</div>
+	</div>
 </div>
-
-<?php endwhile; 
-
-else: ?>
-<p>Sorry, no posts matched your criteria.</p>
-
-
-<?php endif; ?>
-</div>
-</section>
-
-
-<?php get_sidebar(); ?>
+<div id="delimiter"></div>
 <?php get_footer(); ?>
