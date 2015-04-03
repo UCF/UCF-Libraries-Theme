8b92a65aa9c1a75e2fc234a78bbdd7791edba935
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-04-03 15:28:40 -0400

    adding in image.

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 		<div class="col-sm-9">
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			<h1><?php the_title(); ?></h1>
+			<p><?php the_post_thumbnail( ); ?></p>
 			<h4>Posted on <?php the_time('F jS, Y') ?></h4>
 			<p><?php the_content(__('(more...)')); ?></p>
 			<hr> <?php endwhile; else: ?>
