b91efe390c55b07952ea3745f91b101646bc3cd3
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-04-06 13:10:44 -0400

    adding in meta and department to archive-staff. Reordering info on both.

--- archive-staff.php
+++ archive-staff.php
@@ -1,15 +1,18 @@
 <?php get_header(); ?>
 <div id="main">
 	<div id="content" class="container">
+		<h1><?php $term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) ); echo $term->name; ?></h1>
 		<div class="col-sm-3">
 			<?php get_sidebar(); ?>
 		</div>
 		<div class="col-sm-9">
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			<h1><?php the_title(); ?></h1>
-			<p><?php the_post_thumbnail( ); ?></p>
-			<h4>Posted on <?php the_time('F jS, Y') ?></h4>
-			<p><?php the_content(__('(more...)')); ?></p>
+			<div class"col-sm-4">
+				<p><?php the_post_thumbnail( ); ?></p>
+				<h2><?php the_title(); ?></h2>
+				<p><?php the_meta(); ?></p>
+				<p><?php echo get_the_term_list( $post->ID, 'department', 'Department: ', ', ', '' ); ?></p>
+			</div>
 			<hr> <?php endwhile; else: ?>
 			<p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
 		</div>
--- single-staff.php
+++ single-staff.php
@@ -18,8 +18,8 @@ Description: Single staff member page.
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 				<h1><?php the_title(); ?></h1>
 				<p><?php the_post_thumbnail( ); ?></p>
-				<p><?php echo get_the_term_list( $post->ID, 'department', 'Department: ', ', ', '' ); ?></p>
 				<p><?php the_meta(); ?></p>
+				<p><?php echo get_the_term_list( $post->ID, 'department', 'Department: ', ', ', '' ); ?></p>
 				<p><?php the_content(__('(more...)')); ?></p>
 				<hr> <?php endwhile; else: ?>
 				<p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
