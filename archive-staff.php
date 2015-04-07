e7c7aded98314d5b18c98c7a48831397ebd2e5f3
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-04-07 16:16:23 -0400

    added in glyphicons and new formating to archives page

--- archive-staff.php
+++ archive-staff.php
@@ -1,3 +1,10 @@
+<?php
+/*
+Description: Archive staff member page.
+*/
+?>
+
+
 <?php get_header(); ?>
 <div id="main">
 	<div id="content" class="container">
@@ -9,11 +16,32 @@
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			<div class="col-sm-6 col-md-4">
     			<div class="thumbnail">
-					<?php the_post_thumbnail( ); ?>
+    				<figure><a href="<?php echo get_permalink(); ?>"><?php the_post_thumbnail( ); ?></a></figure>
 					<div class="caption">
-						<h3><?php the_title(); ?></h3>
-						<?php the_meta(); ?>
-						<p><?php echo get_the_term_list( $post->ID, 'department', 'Department: ', ', ', '' ); ?></p>
+						<h3><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h3>
+						<?php if(get_post_meta($post->ID, 'title', true) ||
+							 get_post_meta($post->ID, 'room', true) ||
+							 get_post_meta($post->ID, 'phone', true) || 
+							 get_post_meta($post->ID, 'email', true)
+						): ?>
+
+						<ul>
+							<?php if(get_post_meta($post->ID, 'title', true)): ?>
+							<li><span class="glyphicon glyphicon-user"></span> <?php echo get_post_meta($post->ID, 'title', true); ?></li>
+							<?php endif; ?>
+
+							<?php if(get_post_meta($post->ID, 'room', true)): ?>
+							<li><span class="glyphicon glyphicon-map-marker"></span> <?php echo get_post_meta($post->ID, 'room', true); ?></li>
+							<?php endif; ?>
+
+							<?php if(get_post_meta($post->ID, 'phone', true)): ?>
+							<li><span class="glyphicon glyphicon-earphone"></span> <?php echo get_post_meta($post->ID, 'phone', true); ?></li>
+							<?php endif; ?>
+
+							<?php if(get_post_meta($post->ID, 'email', true)): ?>
+							<li><a href="mailto:<?php echo get_post_meta($post->ID, 'email', true); ?>"><span class="glyphicon glyphicon-envelope"></span> <?php echo get_post_meta($post->ID, 'email', true); ?></a></li>
+							<?php endif; ?>
+						</ul>
 					</div>
 				</div>
 			</div>
