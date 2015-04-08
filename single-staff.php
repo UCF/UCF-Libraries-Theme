6b6e5944c9c89ec78e6effb78ac2e91287a48749
johnaedo <johnaedo@users.noreply.github.com>
2015-04-08 16:12:32 -0400

    Update single-staff.php
    Manipulating the_title() so we display names in a friendlier FirstName LastName format.

--- single-staff.php
+++ single-staff.php
@@ -17,7 +17,7 @@ Description: Single staff member page.
 			<div class="col-sm-9">
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 					<header>
-						<h1>People >> <?php the_title(); ?></h1>
+						<h1>People >> <?php the_title().explode(",")[1]." ".the_title().explode(",")[0]; ?></h1>
 					</header>
 					<article class="staff-info-single clearfix">
 						<figure><?php the_post_thumbnail( ); ?></figure>
