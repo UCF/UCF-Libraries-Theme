9a59eae37fa4a1fc665ace877fcff60595dfa8d2
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-04-09 16:23:39 -0400

    adding in post links to post headers

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		</div>
 		<div class="col-sm-9">
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			<h1><?php the_title(); ?></h1>
+			<h2><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h2>
 			<p><?php the_post_thumbnail( ); ?></p>
 			<h4>Posted on <?php the_time('F jS, Y') ?></h4>
 			<p><?php the_content(__('(more...)')); ?></p>
