e9ec300dcb0b833356a29c0d5741ebc25a373ed5
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-04-14 11:11:58 -0400

    updating style url

--- homepage.php
+++ homepage.php
@@ -4,23 +4,27 @@ Template Name: Home Page
 Description: A Template for the Website homepage.
 */
 ?>
-<style type="text/css">
-<!--
-.background-image::after {
-	background: url(images/<?php echo $selectedBg; ?>) no-repeat;
-}
--->
-</style>
+
 <?php
 /*
 Generate a random backround from the list below.
 */
+  $directory = get_template_directory_uri();
   $bg = array('bg-01-main.jpg', 'bg-02-rosen.jpg'); // array of filenames
 
   $i = rand(0, count($bg)-1); // generate random number size of the array
   $selectedBg = "$bg[$i]"; // set variable equal to which random filename was chosen
 ?>
 
+
+<style type="text/css">
+<!--
+.background-image::after {
+	background: url(<?php echo $directory.'/images/'.$selectedBg; ?>) no-repeat;
+}
+-->
+</style>
+s
 <?php get_header(); ?>
 <div id="main">
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
