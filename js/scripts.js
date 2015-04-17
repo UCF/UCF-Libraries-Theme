babb719e780adbcad6a127956887ba36235e5b44
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-04-17 17:12:06 -0400

    added in function to target sidebar-collapse and apply unique ids where necessary

--- js/scripts.js
+++ js/scripts.js
@@ -23,13 +23,33 @@ function collapseMenus() {
     $('.menu-toggle .glyphicon').removeClass("glyphicon-minus-sign").addClass("glyphicon-plus-sign");
 }
 
+//Generate a random string for use in ID override
+function get_random_string() {
+    $text = "";
+    $possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for( var i=0; i < 10; i++ )
+        $text += $possible.charAt(Math.floor(Math.random() * $possible.length));
+    return $text;
+}
+
+
 $(document).ready(function(){
-  if ($(window).width() <= 768){  
+  
+  //Collapse the sidebar menus when less than 768
+  if ($(window).width() < 768){  
 	collapseMenus();
 	$menus_open = false;
   } else {
 	$menus_open = true;
   }
+
+  //Locate the ID of the sidebar collapse menus and give them a unique id
+  $('.sidebar-collapse').each(function() {
+  	$id = get_random_string();
+  	$(this).find('.collapse').prop('id', $id);
+  	$(this).find('.menu-toggle').prop('href', '#'+$id);
+  });
+  
 });
 
 $(window).resize(function(){
