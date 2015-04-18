71329bfccd2cd1e1e4d6b03d6a971609e6c75df7
unknown <bobby.ciullo@ucf.edu>
2015-04-17 23:44:41 -0400

    adding in collapsing menu to taxonomy term lists

--- sidebar-staff.php
+++ sidebar-staff.php
@@ -3,12 +3,24 @@
 <aside>	
 	<div id="secondary" class="secondary">
 		<div id="widget-area" class="widget-area" role="complementary">
-			<h4 class="widget-title">Departments</h4>
-			<?php taxonomy_term_list('department'); ?>
-			<h4 class="widget-title">Units</h4>
-			<?php taxonomy_term_list('unit'); ?>
-			<h4 class="widget-title">Groups</h4>
-			<?php taxonomy_term_list('group'); ?>
+			<div class="sidebar-collapse">
+				<h4 class="widget-title"><a class="menu-toggle" data-toggle="collapse" href="#Department" aria-expanded="true" aria-controls="Department"><span class="glyphicon glyphicon-minus-sign" style="float:right"></span>Departments</a></h4>
+				<div class="collapse in" id="Department">
+					<?php taxonomy_term_list('department'); ?>
+				</div>
+			</div>
+			<div class="sidebar-collapse">
+				<h4 class="widget-title"><a class="menu-toggle" data-toggle="collapse" href="#Unit" aria-expanded="true" aria-controls="Unit"><span class="glyphicon glyphicon-minus-sign" style="float:right"></span>Units</a></h4>
+				<div class="collapse in" id="Unit">
+					<?php taxonomy_term_list('unit'); ?>
+				</div>
+			</div>
+			<div class="sidebar-collapse">
+				<h4 class="widget-title"><a class="menu-toggle" data-toggle="collapse" href="#Group" aria-expanded="true" aria-controls="Group"><span class="glyphicon glyphicon-minus-sign" style="float:right"></span>Groups</a></h4>
+				<div class="collapse in" id="Group">
+					<?php taxonomy_term_list('group'); ?>
+				</div>
+			</div>
 		</div><!-- .widget-area -->
 	</div>
 </aside>
