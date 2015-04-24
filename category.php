7c43b8cfea0036da8ee7b89946c9f44b787dcf3b
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-04-24 17:31:47 -0400

    adding category name to h2

--- category.php
+++ category.php
@@ -8,6 +8,8 @@
 				<?php get_sidebar(); ?>
 			</div>
 			<div class="col-sm-9">
+				<h2 class="subpage-title"><?php $term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'category' ) ); echo $term->name; ?></h2>
+
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 				<?php	
 					$categories = get_the_category();
