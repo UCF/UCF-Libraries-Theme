d73d9d7d5143d479cf4d67c289fe7f6a9d799a0e
unknown <bobby.ciullo@ucf.edu>
2015-04-27 11:15:41 -0400

    changed placeholder text to Search Website

--- searchform.php
+++ searchform.php
@@ -3,7 +3,7 @@
 <form role="form" action="<?php bloginfo('siteurl'); ?>/" id="searchform" method="get">
     <label for="s" class="sr-only">Search</label>
     <div class="input-group">
-        <input type="text" class="form-control" id="s" name="s" placeholder="Search"<?php if ( $search_terms !== '' ) { echo ' value="' . $search_terms . '"'; } ?> />
+        <input type="text" class="form-control" id="s" name="s" placeholder="Search Website"<?php if ( $search_terms !== '' ) { echo ' value="' . $search_terms . '"'; } ?> />
         <span class="input-group-btn">
             <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-search"></span></button>
         </span>
