0d0dc9c5b62608b1147c7cc2c72779aebb7de306
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-05-01 13:43:07 -0400

    added breadcrumbs

--- no_sidebar.php
+++ no_sidebar.php
@@ -5,15 +5,24 @@ Description: This page template does not have a sidebar.
 */
 ?>
 
-
 <?php get_header(); ?>
 <div id="main">
 	<div id="content" class="container">
-		<h1><?php the_title(); ?></h1>
-			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			<p><?php the_content(__('(more...)')); ?></p>
-			<hr> <?php endwhile; else: ?>
-			<p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
+	<!-- no_sidebar -->
+	
+		<header><h1><?php the_title(); ?></h1></header>
+		<?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?>
+
+		<div class="row">
+			<div class="col-sm-12">
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<article>
+				<?php the_content(__('(more...)')); ?>
+				</article>
+				<hr> <?php endwhile; else: ?>
+				<p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
+			</div>
+		</div>
 	</div>
 </div>
 <div id="delimiter"></div>
