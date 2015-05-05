4b950f6f83a1b19dcb39b84e33b976f16fc8b519
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-05-05 09:11:32 -0400

    removed parent page link from title.

--- subpage.php
+++ subpage.php
@@ -9,8 +9,7 @@ Description: Use if a page belongs under another page.
 <?php get_header(); ?>
 <div id="main">
 	<div id="content" class="container">
-		<?php $permalink = get_permalink($post->post_parent); ?>
-		<header><h1><a href="<?php echo $permalink; ?>"><?php echo get_the_title($post->post_parent);?></a></h1></header>
+		<header><h1>><?php echo get_the_title($post->post_parent);?></h1></header>
 		<?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?>
 		<div class="row">
 			<div class="col-sm-3">
