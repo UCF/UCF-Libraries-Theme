1c9520d45dbc6caf0dff0402a58e0e52b26ccc5f
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-05-07 10:15:24 -0400

    changed homepage background image style and changed random code from php to jquery

--- homepage.php
+++ homepage.php
@@ -9,27 +9,28 @@ Description: A Template for the Website homepage.
 /*
 Generate a random backround from the list below.
 */
-  $directory = get_template_directory_uri();
-  $bg = array('bg-01-main.jpg', 'bg-02-rosen.jpg'); // array of filenames
-
-  $i = rand(0, count($bg)-1); // generate random number size of the array
-  $selectedBg = "$bg[$i]"; // set variable equal to which random filename was chosen
+  $directory = get_template_directory_uri().'/images/';
 ?>
 
-<style type="text/css">
-<!--
-.background-image::after {
-	background: url(<?php echo $directory.'/images/'.$selectedBg; ?>) no-repeat;
-}
--->
-</style>
+
 <?php get_header(); ?>
 <div id="main">
-<div style="width:200px; margin: 0 auto;"><?php echo $selectedBg.' '.$i; ?></div>
+<div style="width:200px; margin: 0 auto;"></div>
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<?php the_content(__('(more...)')); ?>
 		<?php endwhile; else: ?>
 		<?php _e('Sorry, no posts matched your criteria.'); ?><?php endif; ?>
 </div>
 <div id="delimiter"></div>
+<script type="text/javascript">
+
+$(document).ready(function() {
+	<?php
+	 $directory = get_template_directory_uri().'/images/';
+ 	 echo "var directory = '$directory';";
+	 ?>
+    var images = ['bg-01-main.jpg', 'bg-02-rosen.jpg'];
+    $('.background-image').css({'background-image': 'url('+ directory + images[Math.floor(Math.random() * images.length)] + ')'});
+   });
+</script>
 <?php get_footer(); ?>
