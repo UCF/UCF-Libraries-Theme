9867e7f6bfcaf0f115959959c148f3c606c6b48b
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-05-14 07:54:13 -0400

    added all staff link.

--- sidebar-staff.php
+++ sidebar-staff.php
@@ -3,6 +3,7 @@
 <aside>	
 	<div id="secondary" class="secondary">
 		<div id="widget-area" class="widget-area" role="complementary">
+			<h4><a href="<?php echo get_post_type_archive_link( 'staff' ); ?>">All Staff</a></h4>
 			<div class="sidebar-collapse">
 				<h4 class="widget-title"><a class="menu-toggle" data-toggle="collapse" href="#Department" aria-expanded="true" aria-controls="Department"><span class="glyphicon glyphicon-minus-sign" style="float:right"></span>Departments</a></h4>
 				<div class="collapse in" id="Department">
