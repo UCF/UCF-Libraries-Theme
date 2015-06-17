98cf75ee5af59228fa2e3ecbfc3b8bc8e33a6ce0
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-06-17 14:29:34 -0400

    added in function for scroll to top button

--- js/scripts.js
+++ js/scripts.js
@@ -121,6 +121,27 @@ $(document).ready(function() {
 });
 
 
+// Scroll to Top Button
+//=====================
+
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            $('.scroll-top').fadeIn();
+        } else {
+            $('.scroll-top').fadeOut();
+        }
+    });
+
+    $('.scroll-top').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+
+});
 
 // Ajax test
 //=============
