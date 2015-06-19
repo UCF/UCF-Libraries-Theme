93de63fb6febed0ff801f969f46f0791d359da12
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-06-19 16:14:32 -0400

    fixed title tag that was blank, now displays correctly

--- homepage.php
+++ homepage.php
@@ -3,6 +3,15 @@
 Template Name: Home Page
 Description: A Template for the Website homepage.
 */
+
+add_filter( 'wp_title', 'baw_hack_wp_title_for_home' );
+function baw_hack_wp_title_for_home( $title )
+{
+  if( empty( $title ) && ( is_home() || is_front_page() ) ) {
+    return __( 'Home', 'theme_domain' );
+  }
+  return $title;
+}
 ?>
 
 <?php
