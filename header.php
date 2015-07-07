3e6e09e9c5006862cc20af7b6efe5c65e2bfdcee
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-07-07 16:04:12 -0400

    adding in theme color for chrome mobile users

--- header.php
+++ header.php
@@ -14,6 +14,7 @@
 		<title>Libraries - <?php wp_title( '', true, 'right' ); ?></title>
 		<meta name="viewport" content="width=device-width">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="theme-color" content="#ffcc00">
 		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 		<script type="text/javascript" id="ucfhb-script" src="//universityheader.ucf.edu/bar/js/university-header.js"></script>
 		<?php wp_head(); ?> 
