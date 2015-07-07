041c23bd8f264870e7f6ed72a41bf67dbc789f37
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-07-07 09:11:50 -0400

    added main wrapper around all code

--- 404.php
+++ 404.php
@@ -4,27 +4,29 @@ Description: 404 page not found.
 */
 ?>
 <?php get_header(); ?>
-<div class="jumbotron">
-    <div class="container">
-    	<div class="row">
-            <div class="col-sm-9">
-                <header>
-                    <h1>404 Page not found</h1>
-                </header>
-		    </div>
-            <div class="col-sm-3">
-                <?php get_search_form(); ?>
+<div id="main">
+    <div class="jumbotron">
+        <div class="container">
+        	<div class="row">
+                <div class="col-sm-9">
+                    <header>
+                        <h1>404 Page not found</h1>
+                    </header>
+    		    </div>
+                <div class="col-sm-3">
+                    <?php get_search_form(); ?>
+                </div>
             </div>
-        </div>
-    </div> <!-- .container -->
-</div> <!-- .jumbotron -->
-<div class="container" id="main">
-    <div class="row">
+        </div> <!-- .container -->
+    </div> <!-- .jumbotron -->
+    <div class="container">
+        <div class="row">
 
-        <div class="col-md-12">
-            <p>We're sorry. The page you were looking for could not be found or does not exist.</p>
-            <p>Need to add more to this page.</p>
-        </div> <!-- .col-md-12 -->
-    </div> <!-- .row -->
-</div><!-- .container -->
+            <div class="col-md-12">
+                <p>We're sorry. The page you were looking for could not be found or does not exist.</p>
+                <p>Need to add more to this page.</p>
+            </div> <!-- .col-md-12 -->
+        </div> <!-- .row -->
+    </div><!-- .container -->
+</div><!-- .main -->
 <?php get_footer(); ?>
