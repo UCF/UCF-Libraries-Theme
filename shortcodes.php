a9f48a4d09fe3a1924a033533fe94ed9277b6874
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-07-16 16:27:30 -0400

    converted computer availability to progress bars

--- shortcodes.php
+++ shortcodes.php
@@ -673,6 +673,11 @@ function computer_availability() {
         $machines_total += $location->machinesTotal;
       }
       $machines_available = ($machines_total-$machines_in_use);
+      if ($machines_total != 0) {
+        $percent_available = round(($machines_available/$machines_total)*100);
+      } else {
+        $percent_available == 0;
+      }
       switch ($i) {
         case 1:
           $floor_number = '1st';
@@ -693,10 +698,30 @@ function computer_availability() {
           $floor_number = 'unknown';
           break;
       }
-      if ($machines_available == 0) {
-        $computers_list .= '<dt>'.$floor_number.' Floor <i class="fa fa-desktop"></i>:</dt><dd class="unavailable">'.$machines_available.' of '.$machines_total.' available.</dd>';
+      if ($percent_available > 65) {
+        $computers_list .= '<dt>'.$floor_number.' Floor <i class="fa fa-desktop"></i>:</dt><dd>
+        <div class="progress">
+          <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="'.$percent_available.'" aria-valuemin="0" aria-valuemax="100" style="min-width: 2em; width: '.$percent_available.'%;">
+            '.$machines_available.' / '.$machines_total.'
+          </div>
+        </div>
+        </dd>';
+      } elseif ($percent_available < 33) {
+        $computers_list .= '<dt>'.$floor_number.' Floor <i class="fa fa-desktop"></i>:</dt><dd>
+        <div class="progress">
+          <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="'.$percent_available.'" aria-valuemin="0" aria-valuemax="100" style="min-width: 3em; width: '.$percent_available.'%;">
+            '.$machines_available.' / '.$machines_total.'
+          </div>
+        </div>
+        </dd>';
       } else {
-        $computers_list .= '<dt>'.$floor_number.' Floor <i class="fa fa-desktop"></i>:</dt><dd>'.$machines_available.' of '.$machines_total.' available.</dd>';
+        $computers_list .= '<dt>'.$floor_number.' Floor <i class="fa fa-desktop"></i>:</dt><dd>
+        <div class="progress">
+          <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="'.$percent_available.'" aria-valuemin="0" aria-valuemax="100" style="min-width: 2em; width: '.$percent_available.'%;">
+            '.$machines_available.' / '.$machines_total.'
+          </div>
+        </div>
+        </dd>';
       }
       $i++;
     }
