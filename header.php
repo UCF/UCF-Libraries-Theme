a76aed16f5c5d3df28aabaa3c188796f51999c68
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-07-23 10:47:39 -0400

    added library-body class to body tag

--- header.php
+++ header.php
@@ -20,35 +20,35 @@
 		<script type="text/javascript" id="ucfhb-script" src="//universityheader.ucf.edu/bar/js/university-header.js"></script>
 		<?php wp_head(); ?> 
 	</head>
-	<body <?php body_class(); ?>>
-	<header class="main-header">
-		<nav class="navbar navbar-library navbar-static-top" role="navigation">
-			<div class="container">
-				<!-- Brand and toggle get grouped for better mobile display -->
-				<div class="navbar-header">
-					<button class="navbar-toggle" data-target=".navbar-collapse" data-toggle="collapse" type="button">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-text">Menu</span>
-					</button>
+	<body <?php body_class('library-body'); ?>>
+		<header class="main-header">
+			<nav class="navbar navbar-library navbar-static-top" role="navigation">
+				<div class="container">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button class="navbar-toggle" data-target=".navbar-collapse" data-toggle="collapse" type="button">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-text">Menu</span>
+						</button>
 
-				    <a class="navbar-brand" href="<?php bloginfo('url')?>"><?php bloginfo('name')?></a>
-				</div> 
-				  <!-- Collect the nav links, forms, and other content for toggling --> 
-				    <?php /* Primary navigation */
-						wp_nav_menu( array(
-						  'menu' 				=> 'Header Menu',
-						  'depth'				=> 2,
-						  'container'			=> 'div',
-						  'container_class'		=> 'collapse navbar-collapse',
-						  'menu_class' 			=> 'nav navbar-nav navbar-right',
-						  'fallback_cb'      	=> 'wp_bootstrap_navwalker::fallback',
-						  //Process nav menu using our custom nav walker
-						  'walker' => new wp_bootstrap_navwalker())
-						);
-					?>
-			  </div>
-			</nav>
-	</header>
+					    <a class="navbar-brand" href="<?php bloginfo('url')?>"><?php bloginfo('name')?></a>
+					</div> 
+					  <!-- Collect the nav links, forms, and other content for toggling --> 
+					    <?php /* Primary navigation */
+							wp_nav_menu( array(
+							  'menu' 				=> 'Header Menu',
+							  'depth'				=> 2,
+							  'container'			=> 'div',
+							  'container_class'		=> 'collapse navbar-collapse',
+							  'menu_class' 			=> 'nav navbar-nav navbar-right',
+							  'fallback_cb'      	=> 'wp_bootstrap_navwalker::fallback',
+							  //Process nav menu using our custom nav walker
+							  'walker' => new wp_bootstrap_navwalker())
+							);
+						?>
+				  </div>
+				</nav>
+		</header>
