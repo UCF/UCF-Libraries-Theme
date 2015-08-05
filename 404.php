d91d350d15c5ce70729da90648eb31ad4fe6be97
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-08-05 16:18:09 -0400

    float citronaut to the left.

--- 404.php
+++ 404.php
@@ -22,11 +22,12 @@ Description: 404 page not found.
   <div class="container">
     <div class="row">
       <div class="col-sm-8">
-        <p>We're sorry. Our website was recently updated, and the page you were looking for could not be found.</p>
-        <p>Need help finding something on our new site?  Try starting at the new <a href="<?php bloginfo('url')?>">UCF Libraries homepage</a>. Or simply <a href="<?php bloginfo('url')?>/ask/">Ask Us!</a></p>
-        <div style="width:174px; margin:0 auto;">
+        <div style="width:25%; margin-right: 1em; float: left;">
           <img src="<?php echo get_template_directory_uri() ?>/images/citronaut.png" alt="The Citronaut">
         </div>
+        <p>We're sorry. Our website was recently updated, and the page you were looking for could not be found.</p>
+        <p>Need help finding something on our new site?  Try starting at the new <a href="<?php bloginfo('url')?>">UCF Libraries homepage</a>. Or simply <a href="<?php bloginfo('url')?>/ask/">Ask Us!</a></p>
+        
       </div> <!-- .col-md-9 -->
       <div class="col-sm-4">
         <?php echo do_shortcode("[ask-chat]"); ?>
