c5e2086d3276131579318dcab03e1cab9296ef85
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-10-20 15:57:25 -0400

    added event tracking for website searches

--- searchform.php
+++ searchform.php
@@ -3,9 +3,9 @@
 <form role="form" class="search search-site" action="<?php bloginfo('siteurl'); ?>/" id="searchform" method="get">
     <label for="s" class="sr-only">Search</label>
     <div class="input-group">
-        <input type="text" class="form-control" id="s" name="s" placeholder="Search Libraries Website"<?php if ( $search_terms !== '' ) { echo ' value="' . $search_terms . '"'; } ?> />
+        <input type="text" class="form-control" id="site_search" name="s" placeholder="Search Libraries Website"<?php if ( $search_terms !== '' ) { echo ' value="' . $search_terms . '"'; } ?> />
         <span class="input-group-btn">
-            <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-search"></span></button>
+            <button type="submit" class="btn btn-primary" onclick="__gaTracker(\'send\', \'event\', \'internal-search\', $(\'#site_search\').val(), \'Site Search\');"><span class="glyphicon glyphicon-search"></span></button>
         </span>
     </div> <!-- .input-group -->
 </form>
