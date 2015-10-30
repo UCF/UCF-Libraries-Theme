ed7487f04d378460cf5f44015cf22f360eb5329e
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-10-30 15:56:53 -0400

    added modal window shortcode

--- shortcodes.php
+++ shortcodes.php
@@ -1001,6 +1001,43 @@ function databases() {
 }
 add_shortcode('databases','databases');
 
+/**
+* Modal Window
+* Adds a modal window onto a page.
+*
+* [modal-window id="modal_name" title="Modal Title"]
+*   Body content for modal
+* [/modal-window]
+*
+**/
+function modal_window($atts, $content = null) {
+  extract(shortcode_atts( array(
+      'id' => '',
+      'title' => '',
+  ), $atts ));
+  $output = '
+    <!-- Modal -->
+    <div class="modal fade" id="'.$id.'" tabindex="-1" role="dialog" aria-labelledby="'.$id.'Label">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="'.$id.'Label">'.$title.'</h4>
+          </div>
+          <div class="modal-body">
+            '.do_shortcode($content).'
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  ';
+  return $output;
+}
+add_shortcode('modal-window', 'modal_window');
+
 
 /**
 * Referral page notification
