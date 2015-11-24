b3a67de95ab36153e00e2f4601f08491fa4e5df8
Bobby Ciullo <bobby.ciullo@ucf.edu>
2015-11-24 16:10:16 -0500

    adding in google search to site

--- search.php
+++ search.php
@@ -24,55 +24,21 @@ $results_count = $wp_query->found_posts;
         <div class="row">
 
             <div class="col-md-12">
-                <?php if (have_posts()) : // Results Found ?>
+                <script>
+  (function() {
+    var cx = '014991031871587396159:7dbjtrfgtse';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:searchresults-only></gcse:searchresults-only>
 
-                    <h1><?php _e('Search Results'); ?></h1>
-                    <?php while (have_posts()) : the_post(); ?>
-
-                    <article <?php post_class(); ?>>
-                        <h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-                        <div class="entry">
-                            <p><?php echo wp_trim_words( get_the_excerpt(), 30, '...' ); ?></p>
-                        </div>
-                    </article>
-
-                    <?php endwhile; ?>
-
-                    <ul class="pager">
-                        <li><?php next_posts_link('<i class="icon-chevron-left"></i>&nbsp; Older Results') ?></li>
-                        <li><?php previous_posts_link('Newer Results &nbsp;<i class="icon-chevron-right"></i>') ?></li>
-                    </ul>
-
-                <?php else : // No Results ?>
-
-                    <p><?php _e('Sorry. We couldn&rsquo;t find anything for that search. View one of our site&rsquo;s pages or a recent article below.'); ?></p>
-                    <div class="row">
-                        <div class="col-md-6 posts">
-                            <h3><?php _e('Recent Articles'); ?></h3>
-                            <ul>
-                                <?php
-                                    $args = array(
-                                        'numberposts' => '10',
-                                        'post_status' => 'publish'
-                                    );
-                                    $recent_posts = wp_get_recent_posts( $args );
-                                    foreach( $recent_posts as $recent ) {
-                                        echo '<li><a href="' . get_permalink($recent["ID"]) . '">' . $recent["post_title"] . '</a></li>';
-                                    }
-                                ?>
-                            </ul>
-                        </div> <!-- .posts -->
-                        <div class="col-md-6 pages">
-                            <h3><?php _e('Page Sitemap'); ?></h3>
-                            <ul>
-                                <?php wp_list_pages('title_li=&sort_column=menu_order'); ?>
-                            </ul>
-                        </div> <!-- .pages -->
-                    </div> <!-- .row -->
-
-                <?php endif; ?>
-
-            </div> <!-- .col-md-8 -->
+            </div> <!-- .col-md-12 -->
         </div> <!-- .row -->
     </div><!-- .container -->
 </div><!-- .main -->
--- searchform.php
+++ searchform.php
@@ -1,11 +1,24 @@
 <?php $search_terms = htmlspecialchars( $_GET["s"] ); ?>
 
-<form role="form" class="search search-site" action="<?php bloginfo('siteurl'); ?>/" id="searchform" method="get">
+<script>
+  // (function() {
+  //   var cx = '014991031871587396159:7dbjtrfgtse';
+  //   var gcse = document.createElement('script');
+  //   gcse.type = 'text/javascript';
+  //   gcse.async = true;
+  //   gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+  //       '//cse.google.com/cse.js?cx=' + cx;
+  //   var s = document.getElementsByTagName('script')[0];
+  //   s.parentNode.insertBefore(gcse, s);
+  // })();
+</script>
+
+ <form role="form" class="search search-site" action="<?php bloginfo('siteurl'); ?>/" id="searchform" method="get">
     <label for="s" class="sr-only">Search</label>
     <div class="input-group">
         <input type="text" class="form-control" id="site_search" name="s" placeholder="Search Libraries Website"<?php if ( $search_terms !== '' ) { echo ' value="' . $search_terms . '"'; } ?> />
         <span class="input-group-btn">
             <button type="submit" class="btn btn-primary" onclick="__gaTracker('send', 'event', 'internal-search', $('#site_search').val(), 'Site Search');"><span class="glyphicon glyphicon-search"></span></button>
         </span>
-    </div> <!-- .input-group -->
+    </div>
 </form>
