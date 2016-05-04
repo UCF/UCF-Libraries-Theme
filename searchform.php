d39245fa9678cdd890592d9ec2726ab7a8aefc0f
Bobby Ciullo <bobby.ciullo@ucf.edu>
2016-05-04 09:19:10 -0400

    fixing 508 compliance issues

--- searchform.php
+++ searchform.php
@@ -14,7 +14,7 @@
 </script>
 
  <form role="form" class="search search-site" action="<?php bloginfo('siteurl'); ?>/" id="searchform" method="get">
-    <label for="s" class="sr-only">Search</label>
+    <label for="site_search" class="sr-only">Search</label>
     <div class="input-group">
         <input type="text" class="form-control" id="site_search" name="s" placeholder="Search Libraries Website"<?php if ( $search_terms !== '' ) { echo ' value="' . $search_terms . '"'; } ?> />
         <span class="input-group-btn">
