5e0d966f543bc8096d7ae075e1051873ab6acc4a
Bobby Ciullo <bobby.ciullo@ucf.edu>
2016-05-04 09:14:34 -0400

    correcting 508 errors to bring us up to compliance

--- footer.php
+++ footer.php
@@ -56,11 +56,11 @@
 				</div>
 			</div>
 			<div class="social-btn-group">
-        <a class="facebook-btn" title="Follow us on Facebook" href="https://www.facebook.com/ucflibrary/" target="_blank"><i class="fa fa-facebook-square"></i></a>
-        <a class="twitter-btn" title="Follow us on Twitter" href="https://twitter.com/UCFLibrary"  target="_blank"><i class="fa fa-twitter-square"></i></a>
-        <a class="instagram-btn" title="Follow us on Instagram" href="https://www.instagram.com/ucflibrary/" target="_blank"><i class="fa fa-instagram"></i></a>
-        <a class="youtube-btn" title="Follow us on YouTube" href="https://www.youtube.com/user/UCFLibraries" target="_blank"><i class="fa fa-youtube-square"></i></a>
-        <a class="gplus-btn" title="Follow us on Google+" href="https://plus.google.com/+UCFLibraries" target="_blank"><i class="fa fa-google-plus-square"></i></a>
+        <a class="facebook-btn" title="Follow us on Facebook" href="https://www.facebook.com/ucflibrary/" target="_blank"><i class="fa fa-facebook-square"></i><span class="sr-only">Follow on Facebook</span></a>
+        <a class="twitter-btn" title="Follow us on Twitter" href="https://twitter.com/UCFLibrary"  target="_blank"><i class="fa fa-twitter-square"></i><span class="sr-only">Follow on Twitter</span></a>
+        <a class="instagram-btn" title="Follow us on Instagram" href="https://www.instagram.com/ucflibrary/" target="_blank"><i class="fa fa-instagram"></i><span class="sr-only">Follow us on Instagram</span></a>
+        <a class="youtube-btn" title="Follow us on YouTube" href="https://www.youtube.com/user/UCFLibraries" target="_blank"><i class="fa fa-youtube-square"></i><span class="sr-only">Follow us on YouTube</span></a>
+        <a class="gplus-btn" title="Follow us on Google+" href="https://plus.google.com/+UCFLibraries" target="_blank"><i class="fa fa-google-plus-square"></i><span class="sr-only">Follow us on Google+</span></a>
 			</div>
 			<div class="brand">
 				<img src="<?php echo get_template_directory_uri() ?>/images/libraries-word-mark.png" alt="UCF Libraries"><br>
