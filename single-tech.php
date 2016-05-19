23123123b9ac3380d6ff38355aea7d0fd2c016da
Bobby Ciullo <bobby.ciullo@ucf.edu>
2016-05-19 16:34:22 -0400

    swapped out the placeholder staff directory post meta and term lists for tech lending post meta and term list

--- single-tech.php
+++ single-tech.php
@@ -11,7 +11,7 @@ Description: Single technology page.
 		<!-- single-tech template. -->
 		<div class="row">
 			<div class="col-sm-8">
-				<header><h1>Staff Directory</h1></header>
+				<header><h1>Technology Lending</h1></header>
 				<?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?>
 			</div>
 			<div class="col-sm-4">
@@ -33,31 +33,17 @@ Description: Single technology page.
 								<div class="col-sm-8">
 									<div class="caption">
 									<h2><?php friendly_name(); ?></h2>
-										<?php if(get_post_meta($post->ID, 'title', true) ||
-											 get_post_meta($post->ID, 'room', true) ||
-											 get_post_meta($post->ID, 'phone', true) || 
-											 get_post_meta($post->ID, 'email', true)
-										): ?>
+    								<?php if(get_the_term_list( $post->ID, 'loan_period', true) ||
+    									 get_post_meta($post->ID, 'availability', true)
+    								): ?>
 										<ul>
-											<?php if(get_post_meta($post->ID, 'title', true)): ?>
-												<li><span class="glyphicon glyphicon-user" data-toggle="tooltip" data-placement="right" title="Position"></span> <?php echo get_post_meta($post->ID, 'title', true); ?></li>
-											<?php endif; ?>
+  										<?php if(get_the_term_list( $post->ID, 'loan_period', true)): ?>
+  											<li><i class="fa fa-hourglass" data-toggle="tooltip" data-placement="right" title="Loan Period"></i><?php echo get_the_term_list( $post->ID, 'loan_period', '', ', ', '' ); ?></li>
+  										<?php endif; ?>
 
-											<?php if(get_the_term_list( $post->ID, 'department', true)): ?>
-												<li><i class="fa fa-university" data-toggle="tooltip" data-placement="right" title="Department"></i><?php echo get_the_term_list( $post->ID, 'department', '', ', ', '' ); ?></li>
-											<?php endif; ?>
-
-											<?php if(get_post_meta($post->ID, 'room', true)): ?>
-												<li><span class="glyphicon glyphicon-map-marker" data-toggle="tooltip" data-placement="right" title="Location"></span> <?php echo get_post_meta($post->ID, 'room', true); ?></li>
-											<?php endif; ?>
-
-											<?php if(get_post_meta($post->ID, 'phone', true)): ?>
-												<li><span class="glyphicon glyphicon-phone-alt" data-toggle="tooltip" data-placement="right" title="Phone"></span> <?php echo get_post_meta($post->ID, 'phone', true); ?></li>
-											<?php endif; ?>
-
-											<?php if(get_post_meta($post->ID, 'email', true)): ?>
-												<li><span class="glyphicon glyphicon-envelope" data-toggle="tooltip" data-placement="right" title="Email"></span><a href="mailto:<?php echo get_post_meta($post->ID, 'email', true); ?>"> <span class="ellipsis"> <?php echo get_post_meta($post->ID, 'email', true); ?></span></a></li>
-											<?php endif; ?>
+  										<?php if(get_post_meta($post->ID, 'availability', true)): ?>
+  											<li><i class="fa fa-question-circle" data-toggle="tooltip" data-placement="right" title="Check Availability"></i> <a href="<?php echo get_post_meta($post->ID, 'availability', true); ?>">Check Availability</li>
+  										<?php endif; ?>
 										</ul>
 										<?php endif; ?>
 									</div>
