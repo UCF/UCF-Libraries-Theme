ce234111aa33f0accab1c6e0c4cd55aa2c8bb354
Bobby Ciullo <bobby.ciullo@ucf.edu>
2016-05-25 10:00:57 -0400

    adding grid / list function

--- archive-staff.php
+++ archive-staff.php
@@ -34,7 +34,75 @@ Description: Archive staff member page.
 				</div><!-- col-sm-3 -->
 				<div class="col-sm-9">
 					<h2 class="subpage-title">All Staff</h2>
-					<div class="directory row">
+          <div class="btn-group" data-toggle="buttons" style="margin-bottom: 1em">
+            <label class="btn btn-primary view-button active">
+              <input type="radio" name="views" autocomplete="off" value="grid" checked><i class="fa fa-th"></i> Grid
+            </label>
+            <label class="btn btn-primary view-button">
+              <input type="radio" name="views"  autocomplete="off" value="list"> <i class="fa fa-th-list"></i> List
+            </label>
+          </div>
+          <div id="list_view" class="view">
+            <div class="card">
+              <div class="table-responsive">
+                <table class="table table-striped" style="text-align: left;">
+                  <tbody>
+                    <tr>
+                      <th></th>
+                      <th><span class="glyphicon glyphicon-user"></span> Name</th>
+                      <th><i class="fa fa-bookmark"></i> Title</th>
+                      <th><i class="fa fa-university"></i> Department</th>
+<!--                       <th><i class="fa fa-university"></i> Room</th> -->
+                      <th><span class="glyphicon glyphicon-phone-alt"></span> Phone</th>
+                      <th><span class="glyphicon glyphicon-envelope"></span> Email</th>
+                    </tr>
+                  <?php if ($my_query->have_posts()) : while ($my_query->have_posts()) : $my_query->the_post(); ?>
+                    <tr>
+                      <td><a href="<?php echo get_permalink(); ?>"><?php the_post_thumbnail('thumbnail', array('class' => 'list-thumbnail')); ?></a></td>
+                      <td><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></td>
+                      <td>
+                        <?php if(get_post_meta($post->ID, 'title', true)): ?>
+                          <?php echo get_post_meta($post->ID, 'title', true); ?>
+                        <?php endif; ?>
+                      </td>
+                      <td>
+                        <?php if(get_the_term_list( $post->ID, 'department', true)): ?>
+                          <?php echo get_the_term_list( $post->ID, 'department', '', ', ', '' ); ?>
+                        <?php endif; ?>
+                      </td>
+<!--                       <td>
+                        <?php if(get_post_meta($post->ID, 'room', true)): ?>
+                          <?php echo get_post_meta($post->ID, 'room', true); ?>
+                        <?php endif; ?>
+                      </td>  -->
+                      <td>
+                        <?php if(get_post_meta($post->ID, 'phone', true)): ?>
+                          <?php echo get_post_meta($post->ID, 'phone', true); ?>
+                        <?php endif; ?>
+                      </td>
+                      <td>
+                        <?php if(get_post_meta($post->ID, 'email', true)): ?>
+                          <a href="mailto:<?php echo get_post_meta($post->ID, 'email', true); ?>">Email</a>
+                        <?php endif; ?>
+                      </td>
+                    </tr>
+                  <?php endwhile; else: ?>
+                    <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>                      
+                      <td></td>
+                      <td></td>
+                    </tr>
+                  <?php endif; ?>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+          <div id="grid_view" class="directory row view view-active">
   					<?php $i = 0; ?>
   					<?php if ($my_query->have_posts()) : while ($my_query->have_posts()) : $my_query->the_post(); ?>
   						<?php $i++; ?>
