f83af4800a8a565db78961e32448a800d2f37e7b
Unknown <bobby.ciullo@ucf.edu>
2018-05-21 15:00:33 -0400

    added search stars search form shortcode

--- shortcodes.php
+++ shortcodes.php
@@ -228,6 +228,31 @@ function search_scua($form) {
 }
 add_shortcode('search-scua', 'search_scua');
 
+/**
+* STARS Search bar shortcode
+* Searches STARS repository
+*
+* Example [search-stars]
+**/
+function search_stars($form) {
+    $form = '
+      <form method="get" action="https://stars.library.ucf.edu/do/search/" id="sidebar-search">
+        <fieldset>      
+          <label for="search" class="sr-only">Search </label>          
+          <div class="input-group">
+            <input class="form-control" type="text" name="q" class="search" id="search" placeholder="Search Collection">
+            <input name="fq" type="hidden" value="virtual_ancestor_link:&quot;https://stars.library.ucf.edu/&quot;">
+            <span class="input-group-btn">  
+              <button type="submit" class="btn btn-default" name="query" value="Search"><span class="glyphicon glyphicon-search"></span><span class="sr-only">Search</span></button>
+            </span>        
+          </div>     
+        </fieldset>
+      </form>
+    ';
+    return $form;
+}
+add_shortcode('search-stars', 'search_stars');
+
 
 /**
 * JQuery Homepage tabs
