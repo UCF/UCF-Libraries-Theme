7daa8304e7e891651b2817e2fda5720357dd00b9
Bobby Ciullo <bobby.ciullo@ucf.edu>
2019-04-12 10:45:28 -0400

    adding anatomy cpt

---
+++ single-anatomy.php
@@ -0,0 +1,174 @@
+<?php
+/*
+Description: Single anatomynology page.
+*/
+?>
+
+
+<?php get_header(); ?>
+<div id="main">
+	<div id="title_bar"class="container">
+		<!-- single-anatomy template. -->
+		<div class="row">
+			<div class="col-sm-8">
+				<header><h1>Anatomy Lending</h1></header>
+				<?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?>
+			</div>
+			<div class="col-sm-4">
+				<div class="header-search"><?php get_search_form(); ?></div>
+			</div>
+		</div>
+	</div>
+	<div  class="background-color-gray">
+		<div id="content" class="container">
+			<div class="row">
+				<div id="sidebar" class="col-sm-3">
+					<?php get_sidebar('anatomy'); ?>
+				</div>
+				<div id="content_area" class="col-sm-9">
+					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+						<article>
+							<div class="thumbnail">
+								<div class="row">
+									<div class="col-sm-4">
+										<figure><?php the_post_thumbnail('staff-thumbnail', array('class' => 'staff-thumbnail')); ?></figure>
+									</div>
+									<div class="col-sm-8">
+										<div class="caption">
+											<h2><?php friendly_name(); ?></h2>
+		    								<?php if(get_the_term_list( $post->ID, 'anatomy_type', true) ||
+		    									get_the_term_list( $post->ID, 'library', true) ||
+		    									get_post_meta($post->ID, 'fine-policy', true) ||
+		    									get_post_meta($post->ID, 'availability', true)
+		    								): ?>
+												<ul>
+													<?php if(get_the_term_list( $post->ID, 'anatomy_type', true)): ?>
+	                        <li><i class="fa fa-info-circle" data-toggle="tooltip" data-placement="right" title="Tech Type"></i><?php echo get_the_term_list( $post->ID, 'anatomy_type', '', ', ', '' ); ?></li>
+	                     		<?php endif; ?>
+		                      <?php if(get_the_term_list( $post->ID, 'a_library', true)): ?>
+		                        <li><i class="fa fa-university" data-toggle="tooltip" data-placement="right" title="Library"></i><?php echo get_the_term_list( $post->ID, 'a_library', '', ', ', '' ); ?></li>
+		                      <?php endif; ?>
+		                      <?php if(get_post_meta($post->ID, 'fine-policy', true)): ?>
+		                        <li><i class="fa fa-usd" data-toggle="tooltip" data-placement="right" title="Fine Policy"></i> <a href="<?php echo get_post_meta($post->ID, 'fine-policy', true); ?>">Fine Policy</a></li>
+		                      <?php endif; ?>
+                      		<?php if(get_post_meta($post->ID, 'availability', true)): ?>
+		  											<li><i class="fa fa-check-circle" data-toggle="tooltip" data-placement="right" title="Availability"></i> 
+		  												<div class="progress">
+									              <div id="item_availability_bar" class="progress-bar " role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="min-width: 4em;">
+									                <span class="total-items-available"></span> / <span class="total-items"></span>
+									              </div>
+									            </div>
+		  											</li>
+		  										<?php endif; ?>
+												</ul>
+												<?php endif; ?>
+												<?php 
+													if (get_the_excerpt($post->ID, true)): 
+														the_excerpt();
+													endif;
+												?>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="clearfix"></div>
+							<?php if($post->post_content != "") : ?>
+								<div class="card" style="padding:1em;">
+									<?php the_content(__('(more...)')); ?>
+								</div>
+							<?php endif; ?>
+							<div class="card" style="padding:1em;">
+								<h3 id="item_availability">Item Availability</h3>
+								<?php if(get_post_meta($post->ID, 'availability', true)): ?>
+									<p>There are <strong><span class="total-items-available"></span> <?php friendly_name(); ?>s available</strong> for checkout.</p>
+									<div class="table-responsive">
+										<?php
+											$url = get_post_meta($post->ID, 'availability', true);
+											$content = wp_remote_get($url);
+											if ($content != false) {
+												$first_step = explode( '<div id="divItemDetails">' , $content['body'] );
+												$second_step = explode("</div>" , $first_step[1] );
+												echo $second_step[0];
+											} else {
+												?><p>Item availability is unavailable.</p> <?php
+											}
+										?>
+									</div>
+								<?php else: ?>	
+									<p> This item is not tracked in our availability system. </p>
+								<?php endif; ?>
+							</div>
+						</article>
+					<?php endwhile; else: ?>
+					<p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<script type="text/javascript">
+// Adds all objects with status "Not Checked Out" and prints them into objects with class .total-items-available
+	function availability_status() {
+		var available_items = 0,
+				total_items = 0,
+				percent_available = 0;
+		$('.table').find('tr').each(function (i, el) {
+	    var $tds = $(this).find('td'),
+	        due_date = $tds.eq(3).text(),
+	        item_status = $tds.eq(2).text();
+	    if (item_status != 'Damaged' && item_status != 'Lost - Patron Billed' && item_status != 'In House Repair' && item_status != 'Lost - Missing' && item_status != 'Lost - Paid' && item_status != 'Withdrawn') {
+		    if (due_date == 'Not Checked Out') {
+		    	available_items++;
+		    }
+		  }
+		  total_items++;
+    });
+    total_items = total_items - 3;
+    percent_available = Math.round((available_items / total_items) * 100);
+    
+    if (available_items > 0) {
+      $('#item_availability_bar').addClass('progress-bar-success');
+    } else {
+      $('#item_availability_bar').addClass('progress-bar-warning');
+    }
+
+    $('#item_availability_bar').css('width', percent_available+'%').attr('aria-valuenow', percent_available);   
+    $('.total-items-available').text(available_items);
+    $('.total-items').text(total_items);
+
+	}
+
+// Hides the empty columns that are pulled in from the catalog
+	function hide_empty() {
+		var row = 0;
+		$('.table').find('tr').each(function (i, el) {
+	  	var $tds = $(this).find('td');
+	  	$tds.eq(1).addClass('hide');
+	  	$tds.eq(4).addClass('hide');
+
+	  	if (row < 2) {
+	  		$(this).addClass('hide');
+	  	}
+	  	if (row == 2) {		
+	  		// Convert this row from tds to ths
+	  		$(this).find('td').each(function (){
+	  			$(this).replaceWith('<th>' + $(this).text() + '</th>'); 
+	  		});
+	  		// Hide newly created ths
+	  		var	$ths = $(this).find('th');
+	  		$ths.eq(1).addClass('hide');
+	  		$ths.eq(4).addClass('hide');
+	  	}
+
+	  	row++;
+	  });
+	}
+
+	$(document).ready(function(){
+		$('.table-responsive > table').addClass('table table-striped');
+
+		availability_status();
+		hide_empty();
+	});
+</script>
+<?php get_footer(); ?>
