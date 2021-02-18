cceb384035b77c2b7a8b53e6df4fc19223a011a0
Bobby Ciullo <bobby.ciullo@ucf.edu>
2021-02-18 16:39:18 -0500

    added page title to slug converter

--- functions.php
+++ functions.php
@@ -93,6 +93,19 @@ function cleanup($content){
 	return $content;
 }
 
+//Page title to slug converter
+function title_to_slug($string) {
+  //Lower case everything
+  $string = strtolower($string);
+  //Make alphanumeric (removes all other characters)
+  $string = preg_replace("/[^a-z0-9_\s-]/", "", $string);
+  //Clean up multiple dashes or whitespaces
+  $string = preg_replace("/[\s-]+/", " ", $string);
+  //Convert whitespaces and underscore to dash
+  $string = preg_replace("/[\s_]/", "-", $string);
+  return $string;
+}
+
 function taxonomy_term_list( $taxonomy ) {
   $term_args = array(
     'hide_empty' => false,
