b7ce4907be4d69789adcd1f5a4bccf81fd29b4ba
Bobby Ciullo <bobby.ciullo@ucf.edu>
2021-02-22 16:41:22 -0500

    moved list checkbox to function

--- sidebar-tech.php
+++ sidebar-tech.php
@@ -1,19 +1,25 @@
 
 
+<?php 
+function taxonomy_filter ($taxonomy_name) {
+	$taxonomy_terms = get_terms(['taxonomy' => $taxonomy_name, 'hide_empty' => false,]);
+	foreach($taxonomy_terms as $taxonomy_term) {
+		$slug = str_replace('-amp', '', title_to_slug($taxonomy_term -> name));
+		echo ('<li><label class="filter-checkbox" ><input type="checkbox" id="'.$slug.'" value="'.$slug.'" /> <span>'.$taxonomy_term -> name.'</span></label></li>');
+	}
+}
+?>
+
 <aside>	
 	<div id="secondary" class="secondary taxonomy-filter">
-		<div id="widget-area" class="widget-area" role="complementary">
-			<h4><a href="<?php echo get_post_type_archive_link( 'tech' ); ?>">All Technology</a></h4>
+		<div id="widget-area-2" class="widget-area" role="complementary">
+			<h3>Filters</h3>
+			<p>Click on a box below to filter items that contain that category.</p>
 			<div class="sidebar-collapse">
 				<h4 class="widget-title"><a class="menu-toggle" data-toggle="collapse" href="#Library" aria-expanded="true" aria-controls="Library"><span class="glyphicon glyphicon-minus-sign" style="float:right"></span><i class="fa fa-university"></i> Library</a></h4>
 				<div class="collapse in" id="Library">
 					<ul>
-					<?php
-						$library_terms = get_terms(['taxonomy' => 'library', 'hide_empty' => false,]);
-						foreach($library_terms as $library_term) {
-							echo ('<li><label><input type="checkbox" value="'.str_replace('-amp', '', title_to_slug($library_term -> name)).'" /> '.$library_term -> name.'</label></li>');
-						}
-					?>
+					<?php taxonomy_filter('library');	?>
 					</ul>
 				</div>
 			</div>
@@ -21,12 +27,7 @@
 				<h4 class="widget-title"><a class="menu-toggle" data-toggle="collapse" href="#TechType" aria-expanded="true" aria-controls="Tech Type"><span class="glyphicon glyphicon-minus-sign" style="float:right"></span><i class="fa fa-info-circle" aria-hidden="true"></i> Tech Type</a></h4>
 				<div class="collapse in" id="Tech_Type">
 					<ul>
-						<?php
-							$library_terms = get_terms(['taxonomy' => 'tech_type', 'hide_empty' => false,]);
-							foreach($library_terms as $library_term) {
-								echo ('<li><label><input type="checkbox" value="'.str_replace('-amp', '', title_to_slug($library_term -> name)).'" /> '.$library_term -> name.'</label></li>');
-							}
-						?>
+						<?php taxonomy_filter('tech_type');	?>
 					</ul>
 				</div>
 			</div>
@@ -34,12 +35,7 @@
 				<h4 class="widget-title"><a class="menu-toggle" data-toggle="collapse" href="#Loan_Period" aria-expanded="true" aria-controls="Loan Period"><span class="glyphicon glyphicon-minus-sign" style="float:right"></span><i class="fa fa-hourglass"></i> Loan Period</a></h4>
 				<div class="collapse in" id="Loan_Period">
 					<ul>
-						<?php
-							$library_terms = get_terms(['taxonomy' => 'loan_period', 'hide_empty' => false,]);
-							foreach($library_terms as $library_term) {
-								echo ('<li><label><input type="checkbox" value="'.str_replace('-amp', '', title_to_slug($library_term -> name)).'" /> '.$library_term -> name.'</label></li>');
-							}
-						?>
+						<?php taxonomy_filter('loan_period');	?>
 					</ul>
 				</div>
 			</div>
@@ -47,12 +43,7 @@
 				<h4 class="widget-title"><a class="menu-toggle" data-toggle="collapse" href="#Eligible_User" aria-expanded="true" aria-controls="Eligible User"><span class="glyphicon glyphicon-minus-sign" style="float:right"></span><i class="fa fa-users"></i> Eligible User</a></h4>
 				<div class="collapse in" id="Eligible_User">
 					<ul>
-						<?php
-							$library_terms = get_terms(['taxonomy' => 'eligible_user', 'hide_empty' => false,]);
-							foreach($library_terms as $library_term) {
-								echo ('<li><label><input type="checkbox" value="'.str_replace('-amp', '', title_to_slug($library_term -> name)).'" /> '.$library_term -> name.'</label></li>');
-							}
-						?>
+						<?php taxonomy_filter('eligible_user');	?>
 					</ul>
 				</div>
 			</div>
