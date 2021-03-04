0f2e86200dc94484f2514214606f2765c5b4503f
Bobby Ciullo <bobby.ciullo@ucf.edu>
2021-03-04 15:50:14 -0500

    removed checkboxes and put back in all item links and library term lists

--- sidebar-anatomy.php
+++ sidebar-anatomy.php
@@ -3,40 +3,13 @@
 <aside>	
 	<div id="secondary" class="secondary taxonomy-filter">
 		<div id="widget-area" class="widget-area" role="complementary">
-			<h3>Filters</h3>
-			<p>Click on a box below to filter items that contain that category.</p>
-			<!-- <div class="sidebar-collapse">
+		<h3><a href="<?php echo get_post_type_archive_link( 'anatomy' ); ?>">All Anatomy</a></h3>
+			<div class="sidebar-collapse">
 				<h4 class="widget-title"><a class="menu-toggle" data-toggle="collapse" href="#Library" aria-expanded="true" aria-controls="Library"><span class="glyphicon glyphicon-minus-sign" style="float:right"></span><i class="fa fa-university"></i> Library</a></h4>
 				<div class="collapse in" id="Library">
-					<?php// taxonomy_filter('a_library'); ?>
-				</div>
-			</div> -->
-			<div class="sidebar-collapse">
-				<h4 class="widget-title"><a class="menu-toggle" data-toggle="collapse" href="#Anatomy_Type" aria-expanded="true" aria-controls="Anatomy Type"><span class="glyphicon glyphicon-minus-sign" style="float:right"></span><i class="fa fa-info-circle" aria-hidden="true"></i> Anatomy Type</a></h4>
-				<div class="collapse in" id="Anatomy_Type">
-					<?php taxonomy_filter('anatomy_type'); ?>
+					<?php taxonomy_term_list('a_library'); ?>
 				</div>
 			</div>
 		</div><!-- .widget-area -->
 	</div>
 </aside>
-<script>
-$('.taxonomy-filter').on('change', 'input[type=checkbox]', function() {
-  var $lis = $('.taxonomy');	
-	var $checked =$('input[type=checkbox]:checked');
-  if ($checked.length)
-  {							
-		var selector = '';
-		$($checked).each(function(index, element){                            
-				selector += "[data-category~='" + element.value + "']";                            
-		});                        
-    $lis.hide();                        
-    $lis.filter(selector).show();			   
-  }
-  else
-  {
-    $lis.show();
-  }
-});
-
-</script>
--- sidebar-tech.php
+++ sidebar-tech.php
@@ -1,52 +1,13 @@
 <aside>	
 	<div id="secondary" class="secondary taxonomy-filter">
 		<div id="widget-area-2" class="widget-area" role="complementary">
-			<h3>Filters</h3>
-			<p>Click on a box below to filter items that contain that category.</p>
+		<h3><a href="<?php echo get_post_type_archive_link( 'tech' ); ?>">All Technology</a></h3>
 			<div class="sidebar-collapse">
 				<h4 class="widget-title"><a class="menu-toggle" data-toggle="collapse" href="#Library" aria-expanded="true" aria-controls="Library"><span class="glyphicon glyphicon-minus-sign" style="float:right"></span><i class="fa fa-university"></i> Library</a></h4>
 				<div class="collapse in" id="Library">
-					<?php taxonomy_filter('library');	?>
-				</div>
-			</div>
-			<div class="sidebar-collapse">
-				<h4 class="widget-title"><a class="menu-toggle" data-toggle="collapse" href="#TechType" aria-expanded="true" aria-controls="Tech Type"><span class="glyphicon glyphicon-minus-sign" style="float:right"></span><i class="fa fa-info-circle" aria-hidden="true"></i> Tech Type</a></h4>
-				<div class="collapse in" id="Tech_Type">
-						<?php taxonomy_filter('tech_type');	?>
-				</div>
-			</div>
-			<div class="sidebar-collapse">
-				<h4 class="widget-title"><a class="menu-toggle" data-toggle="collapse" href="#Loan_Period" aria-expanded="true" aria-controls="Loan Period"><span class="glyphicon glyphicon-minus-sign" style="float:right"></span><i class="fa fa-hourglass"></i> Loan Period</a></h4>
-				<div class="collapse in" id="Loan_Period">
-						<?php taxonomy_filter('loan_period');	?>
-				</div>
-			</div>
-			<div class="sidebar-collapse">
-				<h4 class="widget-title"><a class="menu-toggle" data-toggle="collapse" href="#Eligible_User" aria-expanded="true" aria-controls="Eligible User"><span class="glyphicon glyphicon-minus-sign" style="float:right"></span><i class="fa fa-users"></i> Eligible User</a></h4>
-				<div class="collapse in" id="Eligible_User">
-						<?php taxonomy_filter('eligible_user');	?>
+					<?php taxonomy_term_list('library'); ?>
 				</div>
 			</div>
 		</div><!-- .widget-area -->
 	</div>
 </aside>
-<script>
-$('.taxonomy-filter').on('change', 'input[type=checkbox]', function() {
-  var $lis = $('.taxonomy');	
-	var $checked =$('input[type=checkbox]:checked');
-  if ($checked.length)
-  {							
-		var selector = '';
-		$($checked).each(function(index, element){                            
-				selector += "[data-category~='" + element.value + "']";                            
-		});                        
-    $lis.hide();                        
-    $lis.filter(selector).show();			   
-  }
-  else
-  {
-    $lis.show();
-  }
-});
-
-</script>
