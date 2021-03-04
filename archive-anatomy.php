7a4fe8f6d340fc13cdaa95ba6429270379d7c141
Bobby Ciullo <bobby.ciullo@ucf.edu>
2021-03-04 15:51:11 -0500

    stripped tags from tooltips. added pre_check_box function

--- archive-anatomy.php
+++ archive-anatomy.php
@@ -102,14 +102,14 @@ Description: Archive anatomy page.
                       <td>
                         <?php 
                           if(get_the_term_list( $post->ID, 'anatomy_type', true)): 
-                            echo get_the_term_list( $post->ID, 'anatomy_type', '', ', ', '' ); 
+                            echo strip_tags(get_the_term_list( $post->ID, 'anatomy_type', '', ', ', '' )); 
                           endif;
                         ?>
                       </td>
                        <td>
                         <?php 
                           if(get_the_term_list( $post->ID, 'a_library', true)): 
-                            echo get_the_term_list( $post->ID, 'a_library', '', ', ', '' ); 
+                            echo strip_tags(get_the_term_list( $post->ID, 'a_library', '', ', ', '' )); 
                           endif;
                         ?>
                       </td>
@@ -168,10 +168,10 @@ Description: Archive anatomy page.
                     ): ?>
     								<ul>
   										<?php if(get_the_term_list( $post->ID, 'anatomy_type', true)): ?>
-  											<li><i class="fa fa-info-circle" data-toggle="tooltip" data-placement="right" title="Anatomy Type"></i><?php echo get_the_term_list( $post->ID, 'anatomy_type', '', ', ', '' ); ?></li>
+  											<li><i class="fa fa-info-circle" data-toggle="tooltip" data-placement="right" title="Anatomy Type"></i><?php echo strip_tags(get_the_term_list( $post->ID, 'anatomy_type', '', ', ', '' )); ?></li>
   										<?php endif; ?>
                       <?php if(get_the_term_list( $post->ID, 'a_library', true)): ?>
-                        <li><i class="fa fa-university" data-toggle="tooltip" data-placement="right" title="Library"></i><?php echo get_the_term_list( $post->ID, 'a_library', '', ', ', '' ); ?></li>
+                        <li><i class="fa fa-university" data-toggle="tooltip" data-placement="right" title="Library"></i><?php echo strip_tags(get_the_term_list( $post->ID, 'a_library', '', ', ', '' )); ?></li>
                       <?php endif; ?>
                       <?php if(get_post_meta($post->ID, 'fine-policy', true)): ?>
                         <li><i class="fa fa-usd" data-toggle="tooltip" data-placement="right" title="Fines & Policies"></i> <a href="<?php echo get_post_meta($post->ID, 'fine-policy', true); ?>">Fines & Policies</a></li>
@@ -202,8 +202,7 @@ $('#clear_all').on('click', function(){
 $(document).ready( function(){
   $('.lds-spinner').hide();
   $('#grid_view').addClass('view-active');
+  pre_check_box();
 });
-
-</script>
 </script>
 <?php get_footer(); ?>
