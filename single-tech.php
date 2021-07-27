7dedb281e6046cb77eeb41dd42fa57178411965d
Bobby Ciullo <bobby.ciullo@ucf.edu>
2021-07-27 10:30:03 -0400

    added new primo api function and new item availability features.

--- single-tech.php
+++ single-tech.php
@@ -66,7 +66,7 @@ if ( substr($name, -1) == 's') {
 		                      <?php if(get_post_meta($post->ID, 'fine-policy', true)): ?>
 		                        <li><i class="fa fa-usd" data-toggle="tooltip" data-placement="right" title="Fine Policy"></i> <a href="<?php echo get_post_meta($post->ID, 'fine-policy', true); ?>">Fine Policy</a></li>
 		                      <?php endif; ?>
-                      		<?php /* if(get_post_meta($post->ID, 'availability', true)): ?>
+                      		<?php  if(get_post_meta($post->ID, 'availability', true)): ?>
 		  											<li><i class="fa fa-check-circle" data-toggle="tooltip" data-placement="right" title="Availability"></i> 
 		  												<div class="progress">
 									              <div id="item_availability_bar" class="progress-bar " role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="min-width: 4em;">
@@ -74,7 +74,7 @@ if ( substr($name, -1) == 's') {
 									              </div>
 									            </div>
 		  											</li>
-		  										<?php endif; */ ?>
+		  										<?php endif;  ?>
 												</ul>
 												<?php endif; ?>
 												<?php 
@@ -92,28 +92,15 @@ if ( substr($name, -1) == 's') {
 									<?php the_content(__('(more...)')); ?>
 								</div>
 							<?php endif; ?>
-							<!-- <div class="card" style="padding:1em;">
+							 <div class="card" style="padding:1em;">
 								<h3 id="item_availability">Item Availability</h3>
-								<?php // if(get_post_meta($post->ID, 'availability', true)): ?>
-									<p>There <span class="single-plural"></span> <strong><span class="total-items-available"></span> <?php the_title(); ?><span class="s-ending"></span> available</strong> for checkout.</p>
-									<div class="table-responsive">
-										<?php /*
-											$url = get_post_meta($post->ID, 'availability', true);
-											$content = wp_remote_get($url, array( 'timeout' => 120));
-											if ($content != false) {
-												$first_step = explode( '<div id="divItemDetails">' , $content['body'] );
-												$second_step = explode("</table></div>" , $first_step[1] );
-												$third_step = explode("<script>" , $second_step[0] );
-												echo $third_step[0];
-											} else {
-												?><p>Item availability is unavailable.</p> <?php
-											} */
-										?>
-									</div>
-								<?php // else: ?>	
+								<?php  if(get_post_meta($post->ID, 'availability', true)): ?>
+									<p id="item_availability_message">There <span class="single-plural"></span> <strong><span class="total-items-available"></span> <?php the_title(); ?><span class="s-ending"></span> available</strong> for checkout.</p>
+									<?php $availability = primo_availability_api_call(get_post_meta($post->ID, 'availability', true), 100);	?>
+								<?php  else: ?>	
 									<p> This item is not tracked in our availability system. </p>
-								<?php // endif; ?>
-							</div> -->
+								<?php  endif; ?>
+							</div> 
 						</article>
 					<?php endwhile; else: ?>
 					<p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
@@ -122,79 +109,85 @@ if ( substr($name, -1) == 's') {
 		</div>
 	</div>
 </div>
+
+
 <script type="text/javascript">
 // Adds all objects with status "Not Checked Out" and prints them into objects with class .total-items-available
 	function availability_status() {
-		var available_items = 0,
-				total_items = 0,
+		let available_items = <?php	echo($availability[0]); ?>,
+				total_items = <?php	echo($availability[1]); ?>,
 				percent_available = 0;
 				has_s = <?php echo($has_s); ?>;
-		$('.table').find('tr').each(function (i, el) {
-	    var $tds = $(this).find('td'),
-	        due_date = $tds.eq(3).text(),
-	        item_status = $tds.eq(2).text();
-	    if (item_status != 'Damaged' && item_status != 'Lost - Patron Billed' && item_status != 'In House Repair' && item_status != 'Lost - Missing' && item_status != 'Lost - Paid' && item_status != 'Withdrawn') {
-		    if (due_date == 'Not Checked Out') {
-		    	available_items++;
-		    }
-		  }
-		  total_items++;
-    });
-    total_items = total_items - 3;
-		if (total_items < 0){ total_items = 0;}
-    percent_available = Math.round((available_items / total_items) * 100);
-    
-    if (available_items > 0) {
-      $('#item_availability_bar').addClass('progress-bar-success');
-    } else {
-      $('#item_availability_bar').addClass('progress-bar-warning');
-    }
+		// $('.table').find('tr').each(function (i, el) {
+	  //   var $tds = $(this).find('td'),
+	  //       due_date = $tds.eq(3).text(),
+	  //       item_status = $tds.eq(2).text();
+	  //   if (item_status != 'Damaged' && item_status != 'Lost - Patron Billed' && item_status != 'In House Repair' && item_status != 'Lost - Missing' && item_status != 'Lost - Paid' && item_status != 'Withdrawn') {
+		//     if (due_date == 'Not Checked Out') {
+		//     	available_items++;
+		//     }
+		//   }
+		//   total_items++;
+    // });
+		if (total_items != -1) {
+			if (total_items < 0){ total_items = 0;}
+			percent_available = Math.round((available_items / total_items) * 100);
+			
+			if (available_items > 0) {
+				$('#item_availability_bar').addClass('progress-bar-success');
+			} else {
+				$('#item_availability_bar').addClass('progress-bar-warning');
+			}
 
-    $('#item_availability_bar').css('width', percent_available+'%').attr('aria-valuenow', percent_available);   
-    $('.total-items-available').text(available_items);
-    $('.total-items').text(total_items);
-    if (available_items == 1) {
-    	$('.single-plural').text('is');
-    } else {
-    	$('.single-plural').text('are');
-    }
-    if (available_items != 1 && has_s == 0 ){
-    	$('.s-ending').text('s');
-    }
-
+			$('#item_availability_bar').css('width', percent_available+'%').attr('aria-valuenow', percent_available);   
+			$('.total-items-available').text(available_items);
+			$('.total-items').text(total_items);
+			if (available_items == 1) {
+				$('.single-plural').text('is');
+			} else {
+				$('.single-plural').text('are');
+			}
+			if (available_items != 1 && has_s == 0 ){
+				$('.s-ending').text('s');
+			}
+		} else {
+			$('#item_availability_bar').addClass('progress-bar-danger');
+			$('#item_availability_bar').css('width', '100%').attr('aria-valuenow', 100);   
+			$('#item_availability_bar').text("There was an error locating this item's availabilty status.");
+			$('#item_availability_message').text("There was an error locating this item's availabilty status.");
+		}
 	}
 
 // Hides the empty columns that are pulled in from the catalog
-	function hide_empty() {
-		var row = 0;
-		$('.table').find('tr').each(function (i, el) {
-	  	var $tds = $(this).find('td');
-	  	$tds.eq(1).addClass('hide');
-	  	$tds.eq(4).addClass('hide');
+	// function hide_empty() {
+	// 	var row = 0;
+	// 	$('.table').find('tr').each(function (i, el) {
+	//   	var $tds = $(this).find('td');
+	//   	$tds.eq(1).addClass('hide');
+	//   	$tds.eq(4).addClass('hide');
 
-	  	if (row < 2) {
-	  		$(this).addClass('hide');
-	  	}
-	  	if (row == 2) {		
-	  		// Convert this row from tds to ths
-	  		$(this).find('td').each(function (){
-	  			$(this).replaceWith('<th>' + $(this).text() + '</th>'); 
-	  		});
-	  		// Hide newly created ths
-	  		var	$ths = $(this).find('th');
-	  		$ths.eq(1).addClass('hide');
-	  		$ths.eq(4).addClass('hide');
-	  	}
+	//   	if (row < 2) {
+	//   		$(this).addClass('hide');
+	//   	}
+	//   	if (row == 2) {		
+	//   		// Convert this row from tds to ths
+	//   		$(this).find('td').each(function (){
+	//   			$(this).replaceWith('<th>' + $(this).text() + '</th>'); 
+	//   		});
+	//   		// Hide newly created ths
+	//   		var	$ths = $(this).find('th');
+	//   		$ths.eq(1).addClass('hide');
+	//   		$ths.eq(4).addClass('hide');
+	//   	}
 
-	  	row++;
-	  });
-	}
+	//   	row++;
+	//   });
+	// }
 
 	$(document).ready(function(){
 		$('.table-responsive > table').addClass('table table-striped');
-
 		availability_status();
-		hide_empty();
+		// hide_empty();
 	});
 </script>
 <?php get_footer(); ?>
