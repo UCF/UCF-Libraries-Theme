f13cc0f73bb09be3f39efb2a2159ba48ffedd6eb
Bobby Ciullo <bobby.ciullo@ucf.edu>
2022-05-31 13:42:48 -0400

    added course number search and a fix for localhost environements

--- js/textbooks.js
+++ js/textbooks.js
@@ -4,11 +4,23 @@ let query_parameters = '?' + instructor + '&' + course_number;
 const spinner = '<div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>';
 
 function display_textbooks(query_parameters) {
+  // This part is just for making the code work on the WAMP server
+  let current_url = window.location.href;
+  let localhost_domain = '';
+  if (current_url.includes('localhost')){
+    if (current_url.includes('library-test')){
+      localhost_domain = '/library-test';
+    }
+    if (current_url.includes('library_test')){
+      localhost_domain = '/library_test';
+    }
+  }
+  // Normal code starts here
   if (!query_parameters){
     query_parameters = '';
   }
   $.ajax({
-    url: '/library_test/wp-json/textbooks/rest-ajax'+query_parameters
+    url: localhost_domain+'/wp-json/textbooks/rest-ajax'+query_parameters
   }).done(function(data){
     //console.log(data)
     $('#textbook_content').html(data);
@@ -23,7 +35,16 @@ function submit_textbook_query(){
       let instructor = $('#instructor').val();
       console.log(instructor);
       instructor = instructor.replace(/\s/g, '%20');
-      search_query += '?instructor=configured_field_t_instructors='+ instructor;
+      search_query += 'instructor=configured_field_t_instructors='+ instructor;
+    }
+    if ($('#course_number').val()){
+      let course_number = $('#course_number').val();
+      console.log(course_number);
+      course_number = course_number.replace(/\s/g, '%20');
+      search_query += 'course_number=configured_field_t_course_number='+ course_number;
+    }
+    if (search_query.length > 0){
+      search_query = '?'+search_query
     }
     display_textbooks(search_query);
   });
