45fe5d8212b1df45a916e02aa69e64492b676401
Bobby Ciullo <bobby.ciullo@ucf.edu>
2022-06-01 15:24:04 -0400

    added dynamic select option list that uses the page's course_departments custom field

--- textbook-api.php
+++ textbook-api.php
@@ -5,27 +5,54 @@ add_shortcode('textbook-objects-display', 'textbook_objects_display');
 
 function textbook_objects_display() {
   wp_enqueue_script( 'textbook-rest-ajax-script', get_template_directory_uri().'/js/textbooks.js', 'jquery');
-  
+  //This allows us to add a custom field to the textbook page and put all the select options into that field.
+  global $post;
+  $option_list ='';
+  if (get_post_meta($post->ID, 'course_departments', true)){
+    $select_options = explode(', ', get_post_meta($post->ID, 'course_departments', true));
+    foreach ($select_options as $select_option){
+      $option_list .= '<option>'.$select_option.'</option>';
+    }
+  }
   $output = '<div class="background-color-gray">
               <div class="container">
                 <div class="textbooks-search-box">
                   <div class="card">
                     <form>
-                      <label for="course_number">Course Number</label>
-                      <input name="course_number" id="course_number" type="text">
-                      <label for="book_title">Book Title</label>
-                      <input name="book_title" id="book_title" type="text">
-                      <label for="instructor">Instructor</label>
-                      <input name="instructor" id="instructor" type="text">
+                      <div class="row">
+                        <div class="form-group col-md-6">
+                          <label for="course_number">Course Number</label>
+                          <input class="form-control" name="course_number" id="course_number" type="text">
+                        </div>
+                        <div class="form-group col-md-6">
+                          <label for="course_title">Course Title</label>
+                          <input class="form-control" name="course_title" id="course_title" type="text">
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="form-group col-md-6">
+                          <label for="course_department">Course Department</label>
+                          <select class="form-control" name="course_department" id="course_department">
+                            <option></option>
+                            '.$option_list.'
+                          </select>
+                        </div>
+                        <div class="form-group col-md-6">
+                          <label for="instructor">Instructor</label>
+                          <input class="form-control" name="instructor" id="instructor" type="text">
+                        </div>
+                      </div>
                     </form>
-                    <button title="submit query" class="btn btn-primary" id="submit_query">Submit</button> <button title="Clear Search" class="btn btn-default" id="clear_query">Clear</button>
+                    <div class="clearfix"></div>
+                    <button title="submit query" class="btn btn-primary" id="submit_query">Submit</button> <button type="reset" title="Clear Search" class="btn btn-default" id="clear_query">Clear</button>
                   </div>
                 </div>
                 <div id="textbook_content">
                   <div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
                 </div>
               </div>
-             </div>';
+             </div>';    
+
   return $output;  
 
 }
@@ -54,8 +81,11 @@ function textbook_rest_ajax_callback($request){
   if ($request->get_param('course_number')) {
     $query_params .= '&'.$request->get_param('course_number');
   }
-  if ($request->get_param('book_title')) {
-    $query_params .= '&'.$request->get_param('book_title');
+  if ($request->get_param('course_department')) {
+    $query_params .= '&'.$request->get_param('course_department');
+  }
+  if ($request->get_param('course_title')) {
+    $query_params .= '&'.$request->get_param('course_title');
   }
    $textbooks_json = textbook_items_object($query_params);
    $content =  textbook_object_content($textbooks_json);
