4316b33c7458fa0f7e58e7df22c51d0ccbefd7fa
Bobby Ciullo <bobby.ciullo@ucf.edu>
2023-07-27 11:48:07 -0400

    new layout and background color for occuspace short code

--- shortcodes.php
+++ shortcodes.php
@@ -1507,6 +1507,14 @@ function tech_lending_items ($atts){
 add_shortcode('tech-lending-items', 'tech_lending_items');
 
 
+/* 
+Occuspace Display
+Shows business of a given area within the library
+
+[occuspace-display id="1781"]
+
+*/
+
 function occuspace_display($atts){
   extract(shortcode_atts( array(
     'id' => '1781',
@@ -1514,16 +1522,42 @@ function occuspace_display($atts){
   $json_o = occuspace_api_call($id);
   $output = '';
   if ($json_o !== null) {
+    $output .= '<h2>'.$json_o->data->name.'</h2>
+                <div class="grid">';
     foreach ($json_o->data->childCounts as $floor) {
-      $output .= '<h2>'.$floor->name.'</h2>';
-      $output .= '<p>'.($floor->percentage*100).'% Occupied</p><br>';
+      if ($floor->isActive !== false) {
+        $percent = $floor->percentage;
+        if ($percent < .5) {
+          $busy_class = 'not-busy';
+        } else if ($percent > .8) {
+          $busy_class = 'very-busy';
+        } else {
+          $busy_class = 'busy';
+        }
+        $output .= '<div class="grid-item"><div class="card '.$busy_class.'" style="padding: 0 1em;">';
+        $output .= '<h3>'.$floor->name.'</h3>';
+        switch ($busy_class){
+          case 'not-busy':
+            $output .= '<h4>Not Busy</h4>';
+            break;
+          case 'busy':
+            $output .= '<h4>Busy</h4>';
+            break;
+          case 'very-busy':
+            $output .= '<h4>Very Busy</h4>';
+            break;
+        }
+        $output .= '<p>'.($floor->percentage*100).'% Occupied</p>';
+        $output .= '</div></div>';
+      }
     }
+    $output .= '</div>';
   } else {
     $output .= '<p>Null was returned</p>';
   }
   return $output;
 }
-add_shortcode('occuspace-diplay', 'occuspace_display');
+add_shortcode('occuspace-display', 'occuspace_display');
 
 
 
